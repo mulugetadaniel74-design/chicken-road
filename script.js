a7c19450f0f8cea1a226cbcbe6a466b5ba724b91
mulugetadaniel74-design <mulugetadaniel74@gmail.com>
2026-03-22 23:52:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,14 @@
-let chicken = document.getElementById('chicken');
-let gameArea = document.getElementById('game-area');
-let scoreDisplay = document.getElementById('score-value');
-let y = 20; let x = 50; let score = 0;
+let currentBet = 20; // መነሻ ውርርድ
 
-function move(dir) {
-    if (dir === 'up') y += 30;
-    if (dir === 'down' && y > 20) y -= 30;
-    if (dir === 'left' && x > 5) x -= 10;
-    if (dir === 'right' && x < 90) x += 10;
-    chicken.style.bottom = y + 'px';
-    chicken.style.left = x + '%';
-    score += 1;
-    scoreDisplay.innerText = score;
+function changeBet(amount) {
+    currentBet += amount;
+    
+    // ከ 20 ብር በታች እንዳይወርድ መቆጣጠሪያ
+    if (currentBet < 20) {
+        currentBet = 20;
+        alert("ዝቅተኛው የውርርድ መጠን 20 ብር ነው!");
+    }
+    
+    // በስክሪኑ ላይ እንዲታይ ማድረግ
+    document.getElementById('bet-amount').innerText = currentBet;
 }
-
-function createCar() {
-    let car = document.createElement('div');
-    car.innerHTML = "🚗";
-    car.style.position = "absolute";
-    car.style.fontSize = "40px";
-    car.style.left = "-50px";
-    car.style.bottom = (Math.floor(Math.random() * 5) * 60 + 100) + "px";
-    gameArea.appendChild(car);
-
-    let pos = -50;
-    let interval = setInterval(() => {
-        pos += 5;
-        car.style.left = pos + "px";
-        
-        let cRect = chicken.getBoundingClientRect();
-        let carRect = car.getBoundingClientRect();
-        if (!(cRect.right < carRect.left || cRect.left > carRect.right || cRect.bottom < carRect.top || cRect.top > carRect.bottom)) {
-            alert("ተገጨህ! ነጥብህ: " + score);
-            location.reload();
-        }
-
-        if (pos > window.innerWidth) {
-            clearInterval(interval);
-            car.remove();
-        }
-    }, 30);
-}
-setInterval(createCar, 1500);
