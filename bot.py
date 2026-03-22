4786408afc5599b592b9b78fd04a419c41c10571
mulugetadaniel74-design <mulugetadaniel74@gmail.com>
2026-03-23 00:31:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 import telebot
+import os
 
+# የአዲሱ ቶከንህ ቦታ
 TOKEN = "8398999747:AAGS7-Hc9sjBzBs7vu_Mjj8uiONpwfhxLgw"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, "እንኳን ወደ KENO ROYAL በሰላም መጡ! ዌብሳይቱን ተጠቅመው ይጫወቱ።")
+    bot.reply_to(message, "እንኳን ወደ KENO ROYAL በሰላም መጡ!")
 
 if __name__ == '__main__':
-    bot.remove_webhook() # የቆየውን ግኑኝነት ያጸዳል
-    print("ቦቱ በአዲሱ ቶከን ስራ ጀምሯል...")
+    bot.remove_webhook()
+    # Render ላይ እንዲሰራ ይሄን ይጠቀማል
     bot.polling(none_stop=True)
     
