a626f27816a18781399fc7b7e6b21a20faa8f0bb
mulugetadaniel74-design <mulugetadaniel74@gmail.com>
2026-03-23 00:26:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,14 @@
+import telebot
+
+TOKEN = "8398999747:AAGS7-Hc9sjBzBs7vu_Mjj8uiONpwfhxLgw"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "እንኳን ወደ KENO ROYAL በሰላም መጡ! ዌብሳይቱን ተጠቅመው ይጫወቱ።")
+
 if __name__ == '__main__':
-    # የቀድሞውን ግንኙነት በሙሉ ያጸዳል
-    bot.remove_webhook()
-    print("ቦቱ ስራ ጀምሯል...")
-    # ስህተት ቢመጣ እንኳ ራሱ እንዲያስተካክል
-    bot.polling(none_stop=True, interval=0, timeout=20)
+    bot.remove_webhook() # የቆየውን ግኑኝነት ያጸዳል
+    print("ቦቱ በአዲሱ ቶከን ስራ ጀምሯል...")
+    bot.polling(none_stop=True)
+    
